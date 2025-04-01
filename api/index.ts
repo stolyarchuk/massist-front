8d2eea31008106694bc3921e67a8b48e64c356ce
Roman Stolyarchuk <roman@stolyarch.uk>
2025-04-02 00:57:54 +0400

    chore: update API bindings and configuration for improved endpoint handling

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
-import { Env } from "../src/utils/types";
+import { Bindings } from "../src/utils/types";
 
-const app = new Hono<{ Bindings: Env }>();
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/api/", async (c) => {
   return c.json({ name: "Cloudflare" });
